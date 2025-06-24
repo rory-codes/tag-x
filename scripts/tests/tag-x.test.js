d749ad9619827d5b433b3d98d259ac53c8a7c8ba
rory-codes <rory.thompson@outlook.com>
2025-06-24 16:58:46 +0100

    Setup jest environment for testing

---
+++ scripts/tests/tag-x.test.js
@@ -0,0 +1,13 @@
+/** 
+ * @jest-environment jsdom
+ */
+
+//Import the function to be tested
+const {  } = require('../tag-x');
+
+//Test environment + Load HTML
+beforeAll(() => {
+    let fs = require('fs');
+    let fileContents = fs.readFileSync('index.html', 'utf8');
+    document.body.innerHTML = fileContents;
+});
